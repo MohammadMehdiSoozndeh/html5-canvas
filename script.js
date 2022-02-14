1011fee3403ff4e3917f9c08e7c54c4a82ed36a2
Mohamad Moein Hajesmaeili <mohamadmoein.h@gmail.com>
2022-02-14 11:15:37 +0330

    Store video from canvas, finish particles system

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const canvas = document.getElementById('my-canvas')
+const video = document.getElementById('my-video')
 const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
 const particlesArray = []
-
-window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-})
+let hue = 0
 
 const mouse = {
   x: undefined,
@@ -37,6 +32,7 @@ class Particle {
 		this.size = Math.random() * 12 + 1
 		this.speedX = Math.random() * 2 - 1
 		this.speedY = Math.random() * 2 - 1
+		this.color = `hsl(${hue}, 100%, 50%)`
 	}
 
 	update() {
@@ -46,7 +42,7 @@ class Particle {
 	}
 
 	draw() {
-		ctx.fillStyle = 'white'
+		ctx.fillStyle = this.color
 		ctx.beginPath()
 		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
 		ctx.fill()
@@ -66,9 +62,29 @@ function handleParticles() {
 
 function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	// ctx.fillStyle = 'rgba(0,0,0,0.02)'
+	// ctx.fillRect(0, 0,s canvas.width, canvas.height)
 	handleParticles()
+	hue += 0.5
 	requestAnimationFrame(animate)
 }
 
 animate()
 
+// var videoStream = canvas.captureStream(60);
+// var mediaRecorder = new MediaRecorder(videoStream);
+
+// var chunks = [];
+// mediaRecorder.ondataavailable = function(e) {
+//   chunks.push(e.data);
+// };
+
+// mediaRecorder.onstop = function(e) {
+//   var blob = new Blob(chunks, { 'type' : 'video/mp4' });
+//   chunks = [];
+//   var videoURL = URL.createObjectURL(blob);
+//   video.src = videoURL;
+// };
+
+// mediaRecorder.start();
+// setTimeout(function (){ mediaRecorder.stop(); }, 5000);
